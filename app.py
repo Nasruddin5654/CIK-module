48fca5f586bc568b26aeafedc1f508b228bee066
Nasruddin <Nasruddinm337@gmail.com>
2025-07-31 22:10:33 -0400

    Added name_to_cik and ticker_to_cik that returns tuple of CIK, Name, Ticker

--- app.py
+++ app.py
@@ -6,16 +6,48 @@ class SecEdgar:
         self.namedoct={}
         self.tickerdict={}
 
-        headers={"user-agent": 'MLT fellow nasruddin@mlt.org'}
+        headers={"user-agent": 'MLT fellow name@mlt.org'}
         r=requests.get(self.fileurl, headers=headers)
-        self.filejson=r.json
-        print(r.text)
-        print(self.filejson)
+        self.filejson=r.json()
+        
 
         self.cik_json_to_dict()
 
     def cik_json_to_dict(self):
-        self.name_dict={}
-        self.ticker_dict={}
+        self.name_dict = {} #empty dicts
+        self.ticker_dict = {}
 
-se=SecEdgar('https://www.sec.gov/files/company_tickers.json')
+        for data in self.filejson.values():
+            #iterates through the file and initalizes cik, ticker and name
+            cik = data.get("cik_str")
+            ticker = data.get("ticker")
+            name = str(data.get("title"))
+            #dict's name as a key and returns cik or ticker as a value
+            self.name_dict[name] = {"name": name, "ticker": ticker, "cik":cik} 
+
+
+            #finds cik or name by searching it up with the ticker
+            self.ticker_dict[ticker]={"ticker":ticker ,"name": name, "cik": cik} 
+       
+    # method that returns tuple of CIK, NAME, TICKER
+    def name_to_cik(self, name):
+        if name in self.name_dict:
+            data = self.name_dict[name]
+            return (data["cik"], name, data["ticker"])
+        else:
+            return None
+        
+    def ticker_to_cik(self, ticker):
+        if ticker in self.ticker_dict:
+            data = self.ticker_dict[ticker]
+            return (data["cik"], data["name"], ticker)
+        else:
+            return None
+    
+    
+
+se = SecEdgar('https://www.sec.gov/files/company_tickers.json')
+
+#test cases
+print(se.name_to_cik("Apple Inc."))
+print(se.ticker_to_cik("MSFT"))
